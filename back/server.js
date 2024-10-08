e9f4dcc0662a9445fe20b7e3c9d1013052a6d265
fegy <fedorcak.jan@gmail.com>
2024-10-08 13:48:46 +0200

    Update server.js

--- back/server.js
+++ back/server.js
@@ -1,18 +1,6 @@
-const express = require('express');
-const path = require('path');
+const app = require('./app');
+const port = process.env.PORT || 5000;
 
-const app = express();
-const PORT = process.env.PORT || 5000; // Nastav port na 5000 alebo použij port z prostredia
-
-// Nastav statické súbory na servírovanie z build priečinka
-app.use(express.static(path.join(__dirname, '../front/build')));
-
-// Route pre obsluhu hlavnej stránky
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../front/build/index.html'));
-});
-
-// Spusti server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
