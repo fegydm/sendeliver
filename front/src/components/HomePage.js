ba19d3859f29f7337a37ec470f33af6ca454c3a0
fegy <fedorcak.jan@gmail.com>
2024-10-08 15:45:43 +0200

    Update HomePage.js

--- front/src/components/HomePage.js
+++ front/src/components/HomePage.js
@@ -1,1 +1,17 @@
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Sendeliver</h1>
+      <p>Please choose your portal:</p>
+      <ul>
+        <li><Link to="/sender">Sender Portal</Link></li>
+        <li><Link to="/hauler">Hauler Portal</Link></li>
+      </ul>
+    </div>
+  );
+}
+
+export default HomePage;
 
