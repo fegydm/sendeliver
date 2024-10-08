2bc289fc4492c278141cc5237de83f36506c949c
fegy <fedorcak.jan@gmail.com>
2024-10-08 15:52:21 +0200

    Update HaulerPage.js

--- front/src/components/HaulerPage.js
+++ front/src/components/HaulerPage.js
@@ -1,1 +1,12 @@
+function HaulerPage() {
+  return (
+    <div>
+      <h1>Hauler Portal</h1>
+      <p>Welcome to the Hauler portal of Sendeliver!</p>
+      {/* Ďalší obsah pre Hauler */}
+    </div>
+  );
+}
+
+export default HaulerPage;
 
