e777f1b6889fdd15a6266bad76a861ba5028615d
fegy <fedorcak.jan@gmail.com>
2024-10-08 15:50:40 +0200

    Create SenderPage.js

---
+++ front/src/components/SenderPage.js
@@ -0,0 +1,11 @@
+function SenderPage() {
+  return (
+    <div>
+      <h1>Sender Portal</h1>
+      <p>Welcome to the Sender portal of Sendeliver!</p>
+      {/* Ďalší obsah pre Sender */}
+    </div>
+  );
+}
+
+export default SenderPage;
