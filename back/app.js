1d19eadaf61bb34d90a972f97eceb655e1b22a3d
fegy <fedorcak.jan@gmail.com>
2024-10-08 13:49:23 +0200

    Update app.js

--- back/app.js
+++ back/app.js
@@ -1,1 +1,14 @@
+// Import Express frameworku
+const express = require('express');
+const app = express();
+
+// Middleware pre spracovanie JSON requestov
+app.use(express.json());
+
+// Základná routa pre testovanie
+app.get('/', (req, res) => {
+  res.send('Hello from the app!');
+});
+
+module.exports = app;
 
