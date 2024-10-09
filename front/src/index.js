90a6bf45270b3388652daa002c4aa18d6fcd2dad
fegy <fedorcak.jan@gmail.com>
2024-10-09 15:35:30 +0200

    Update index.js

--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './routes/App';  // Odkaz na App.js vo /routes
+import './index.css'; // Ak máš nejaké globálne štýly
 
 ReactDOM.render(
   <React.StrictMode>
