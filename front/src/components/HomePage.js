f4d511bc574bf7c063536bd5c1ae6da85940f6dc
FEGY <fedorcak.jan@gmail.com>
2024-10-09 18:29:23 +0200

    rozdvojnik front a tailwind

--- front/src/components/HomePage.js
+++ front/src/components/HomePage.js
@@ -1,17 +1,20 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
 
 function HomePage() {
   return (
-    <div>
-      <h1>Welcome to Sendeliver</h1>
-      <p>Please choose your portal:</p>
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-3xl font-bold">Welcome to Sendeliver</h1>
+      <p className="mt-4">Please choose your portal:</p>
       <ul>
-        <li><Link to="/sender">Sender Portal</Link></li>
-        <li><Link to="/hauler">Hauler Portal</Link></li>
+        <li className="mt-2">
+          <a href="/sender" className="text-blue-300 hover:text-blue-100">Sender Portal</a>
+        </li>
+        <li className="mt-2">
+          <a href="/hauler" className="text-blue-300 hover:text-blue-100">Hauler Portal</a>
+        </li>
       </ul>
     </div>
   );
 }
 
 export default HomePage;
-
--- front/src/routes/App.js
+++ front/src/routes/App.js
@@ -1,25 +1,17 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import SenderPage from './components/SenderPage';
-import HaulerPage from './components/HaulerPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from '../components/HomePage';
+import SenderPage from '../components/SenderPage';
+import HaulerPage from '../components/HaulerPage';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        {/* Hlavná stránka */}
-        <Route exact path="/" component={HomePage} />
-        
-        {/* Podstránky pre sender a hauler */}
-        <Route path="/sender" component={SenderPage} />
-        <Route path="/hauler" component={HaulerPage} />
-
-        {/* Prípadná podpora pre subdomény */}
-        <Route path="*.sender.sendeliver.com" component={SenderPage} />
-        <Route path="*.hauler.sendeliver.com" component={HaulerPage} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sender" element={<SenderPage />} />
+        <Route path="/hauler" element={<HaulerPage />} />
+      </Routes>
     </Router>
   );
 }
-
 export default App;
