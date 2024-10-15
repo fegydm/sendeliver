d49fccabb13922b545428bf6c7b8ff2fff78f171
fegy <fedorcak.jan@gmail.com>
2024-10-15 19:40:28 +0200

    Update App.js podstranky

--- front/src/routes/App.js
+++ front/src/routes/App.js
@@ -1,17 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from '../components/HomePage';
-import SenderPage from '../components/SenderPage';
-import HaulerPage from '../components/HaulerPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import SenderPage from './components/SenderPage';
+import HaulerPage from './components/HaulerPage';
 
 function App() {
+  const domain = window.location.hostname;
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/sender" element={<SenderPage />} />
-        <Route path="/hauler" element={<HaulerPage />} />
-      </Routes>
+      <Switch>
+        {/* Zobrazovanie podľa domény */}
+        {domain.includes('sender') ? (
+          <Route exact path="/" component={SenderPage} />
+        ) : domain.includes('hauler') ? (
+          <Route exact path="/" component={HaulerPage} />
+        ) : (
+          <Route exact path="/" component={HomePage} />
+        )}
+      </Switch>
     </Router>
   );
 }
+
 export default App;
+
