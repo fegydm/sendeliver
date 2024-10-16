2c27b85e770d94ab9618ae27a54f76a7293adec5
fegy <fedorcak.jan@gmail.com>
2024-10-16 09:54:33 +0200

    Update App.js

--- front/src/routes/App.js
+++ front/src/routes/App.js
@@ -1,15 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from '../components/HomePage';
-import SenderPage from '../components/SenderPage';
-import HaulerPage from '../components/HaulerPage';
+import HomePage from './components/HomePage';
+import SenderPage from './components/SenderPage';
+import HaulerPage from './components/HaulerPage';
 
 function App() {
+  const domain = window.location.hostname;
+
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/sender" element={<SenderPage />} />
-        <Route path="/hauler" element={<HaulerPage />} />
+        {domain.includes('sender') ? (
+          <Route exact path="/" element={<SenderPage />} />
+        ) : domain.includes('hauler') ? (
+          <Route exact path="/" element={<HaulerPage />} />
+        ) : (
+          <>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/sender" element={<SenderPage />} />
+            <Route path="/hauler" element={<HaulerPage />} />
+          </>
+        )}
       </Routes>
     </Router>
   );
