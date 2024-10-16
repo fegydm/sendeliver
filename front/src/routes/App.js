aa9071022a0b37e76fd1abdd879590d7b64932a2
fegy <fedorcak.jan@gmail.com>
2024-10-16 11:40:03 +0200

    pridanie riadku console

--- front/src/routes/App.js
+++ front/src/routes/App.js
@@ -5,6 +5,7 @@ import HaulerPage from '../components/HaulerPage';
 
 function App() {
   const domain = window.location.hostname;
+  console.log("Current Domain: ", domain);
 
   return (
     <Router>
