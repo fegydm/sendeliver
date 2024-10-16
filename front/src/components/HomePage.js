9c4b4ffa2b2c7ee537730acef9ca5ee44599ae68
fegy <fedorcak.jan@gmail.com>
2024-10-16 14:16:06 +0000

    Automatický commit

--- front/src/components/HomePage.js
+++ front/src/components/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 function HomePage() {
   return (
     <div className="bg-blue-500 text-white p-4">
-      <h1 className="text-3xl font-bold">Welcome to Sendeliver</h1>
+      <h1 className="text-3xl font-bold">Welcome to Sendeliver DDHP </h1>
       <p className="mt-4">Please choose your portal:</p>
       <ul>
         <li className="mt-2">
