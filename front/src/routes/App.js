5ec3a3c19634ee9cd29a064d9392c8c8222e2d9e
fegy <fedorcak.jan@gmail.com>
2024-10-17 08:09:50 +0000

    Updated App.js for subdomain routing

--- front/src/routes/App.js
+++ front/src/routes/App.js
@@ -6,21 +6,21 @@ import HaulerPage from '../components/HaulerPage';
 function App() {
   const domain = window.location.hostname;
   console.log("Current Domain: ", domain);
+  
   return (
     <Router>
       <Routes>
-        {domain === 'sender.sendeliver.com' ? (
-  <Route exact path="/" element={<SenderPage />} />
-) : domain === 'hauler.sendeliver.com' ? (
-  <Route exact path="/" element={<HaulerPage />} />
-) : (
-  <>
-    <Route exact path="/" element={<HomePage />} />
-    <Route path="/sender" element={<SenderPage />} />
-    <Route path="/hauler" element={<HaulerPage />} />
-  </>
-)}
-
+        {domain === 'sender.sendeliver.com' || domain === 'clients.sendeliver.com' ? (
+          <Route exact path="/" element={<SenderPage />} />
+        ) : domain === 'hauler.sendeliver.com' || domain === 'carriers.sendeliver.com' ? (
+          <Route exact path="/" element={<HaulerPage />} />
+        ) : (
+          <>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/sender" element={<SenderPage />} />
+            <Route path="/hauler" element={<HaulerPage />} />
+          </>
+        )}
       </Routes>
     </Router>
   );
