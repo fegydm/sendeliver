1f1db7c1563d80cff063c1c10ca0490cef73a467
fegy <fedorcak.jan@gmail.com>
2024-10-24 15:51:24 +0200

    Update server.js

--- back/server.js
+++ back/server.js
@@ -1,6 +1,18 @@
-const app = require('./app'); // Načítanie Express aplikácie z app.js
-const port = process.env.PORT || 5000; // Definovanie portu
+// ./back/server.js
+import express from 'express';
+import { setupWebSocket } from './config/websocket.js';
+import { initRedis } from './config/redis.js';
+import { setHeaders, wwwRedirect } from './utils/errorHandler.js';
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const app = express();
+const server = http.createServer(app);
+
+// Headers a www redirect
+app.use(setHeaders);
+app.use(wwwRedirect);
+
+// Initialize WebSocket & Redis
+setupWebSocket(server);
+initRedis();
+
+server.listen(process.env.PORT || 3000);
