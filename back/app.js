4b6dc6654c374b81f33ab1c7eae759095f0462e6
fegy <fedorcak.jan@gmail.com>
2024-10-24 19:04:46 +0000

    up websocket

--- back/app.js
+++ back/app.js
@@ -3,12 +3,19 @@ import express from 'express';
 
 const app = express();
 
-// Basic test endpoint
+// Enable WebSocket upgrade
+app.use((req, res, next) => {
+  res.setHeader('Upgrade', 'websocket');
+  res.setHeader('Connection', 'Upgrade');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
-// WebSocket test endpoint
 app.get('/ws', (req, res) => {
   res.send('WebSocket endpoint ready');
 });
--- back/config/websocket.js
+++ back/config/websocket.js
@@ -1,27 +1,23 @@
 // ./back/config/websocket.js
 import { WebSocketServer } from 'ws';
-import { handleMessage } from '../services/wsService.js';
 
 export const setupWebSocket = (server) => {
   const wss = new WebSocketServer({ 
     server,
     path: '/ws',
-    perMessageDeflate: false
+    perMessageDeflate: false,
+    handleProtocols: () => 'echo-protocol'
   });
   
-  wss.on('connection', (ws, request) => {
-    console.log('New WebSocket connection:', {
-      path: request.url,
-      headers: request.headers
+  wss.on('connection', (ws) => {
+    console.log('Client connected');
+    
+    ws.send(JSON.stringify({ message: 'Connected!' }));
+    
+    ws.on('message', (message) => {
+      console.log('Received:', message.toString());
+      ws.send(message.toString()); // Echo back
     });
-    
-    ws.on('message', (data) => handleMessage(ws, data));
-    
-    ws.on('error', (error) => {
-      console.error('WebSocket connection error:', error);
-    });
-    
-    ws.send(JSON.stringify({ type: 'welcome', message: 'Connected to WebSocket server' }));
   });
 
   return wss;
