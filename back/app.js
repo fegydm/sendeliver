eeba9566b18bdf96010a7639491bd76c02774b40
fegy <fedorcak.jan@gmail.com>
2024-10-26 12:55:16 +0000

    up back/app.js

--- back/app.js
+++ back/app.js
@@ -1,7 +1,15 @@
 // ./back/app.js
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+// Servuj statické súbory z React buildu
+app.use(express.static(path.join(__dirname, '../front/build')));
 
 // HSTS header pre prevenciu www pri dvojkliku
 app.use((req, res, next) => {
@@ -9,20 +17,9 @@ app.use((req, res, next) => {
   next();
 });
 
-// Basic test endpoint
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
-
-// WebSocket test endpoint
-app.get('/ws', (req, res) => {
-  res.send('WebSocket endpoint ready');
-});
-
-// Log všetkých požiadaviek
-app.use((req, res, next) => {
-  console.log('Incoming request:', req.method, req.url);
-  next();
+// Všetky ostatné GET requesty presmeruj na React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../front/build/index.html'));
 });
 
 export default app;
