1d02d102088f3007e271a188d750f91e6be90886
fegy <fedorcak.jan@gmail.com>
2024-11-07 19:52:02 +0000

    up server

--- back/server.js
+++ back/server.js
@@ -28,5 +28,5 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-    console.log('Server running on http://localhost:', PORT);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
