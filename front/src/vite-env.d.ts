89ec37383e23e46ed4f73748439f97ce430456f5
fegy <fedorcak.jan@gmail.com>
2024-11-07 19:23:32 +0000

    up vite-env

--- front/src/vite-env.d.ts
+++ front/src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
     readonly VITE_API_URL: string;
     readonly VITE_WS_URL: string;
+    readonly PROD: boolean; // Pridanie vlastnosti PROD
 }
 
 interface ImportMeta {
