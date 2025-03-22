435d520e51cc59046551b7df193450107d19202a
FEGY <fedorcak.jan@gmail.com>
2025-03-22 08:38:45 +0100

    up delete js

--- back/src/constants/geo.constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// File: back/src/constants/pagination.constants.ts
-// Last change: Adjusted constants for bulk loading approach
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MAX_QUERY_SIZE = exports.DEFAULT_FETCH_SIZE = void 0;
-// Server-side pagination
-exports.DEFAULT_FETCH_SIZE = 100; // Standard fetch size for locations
-exports.MAX_QUERY_SIZE = 1000; // Maximum allowed query size for safety
--- back/src/types/ai.types.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// File: src/types/ai.types.ts
-// Last change: Enhanced AIRequest and AIResponse types with flexibility for logistics data
-Object.defineProperty(exports, "__esModule", { value: true });
